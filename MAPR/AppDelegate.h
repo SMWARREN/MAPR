b4c15725b0eec7adebd2e15f8c5517baadee5859
Sean Warren <SMWarren@seans-macbook-pro.local>
2016-04-18 17:37:32 -0400

    Initial Commit

---
+++ MAPR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MAPR
+//
+//  Created by Sean Warren on 4/18/16.
+//  Copyright © 2016 Sean Warren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MAPR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MAPR
+//
+//  Created by Sean Warren on 4/18/16.
+//  Copyright © 2016 Sean Warren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
